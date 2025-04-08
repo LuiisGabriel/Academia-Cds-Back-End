cb01ea4175ec957fbad3e91fd298c13c69009016
Luis Gabriel <131626706+LuiisGabriel@users.noreply.github.com>
2025-04-08 16:41:23 -0300

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,6 +4,10 @@ import AuthController from '../controllers/authController.js';
 const router = express.Router();
 const authController = new AuthController();
 
+router.options('*', (req, res) => {
+  res.sendStatus(204);
+});
+
 router.post('/auth/signup', (req, res) => authController.signup(req, res));
 router.post('/auth/signin', (req, res) => authController.signin(req, res));
 router.get('/auth/me', (req, res) => authController.getCurrentUser(req, res));
