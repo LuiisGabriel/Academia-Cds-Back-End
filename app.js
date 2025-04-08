216a5901b4a2268573a485c2af02f28c58e36cc2
Luis Gabriel <131626706+LuiisGabriel@users.noreply.github.com>
2025-04-08 16:46:36 -0300

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ const corsOptions = {
   origin: '*', 
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
   allowedHeaders: ['Content-Type', 'Authorization'], 
-  credentials: true 
+  credentials: true,
+  preflightContinue: false
 };
 
 app.use(cors(corsOptions));
