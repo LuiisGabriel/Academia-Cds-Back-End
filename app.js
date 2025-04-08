4b5d828ae774009371594ce17b0e5b955e32c6e2
Luis Gabriel <131626706+LuiisGabriel@users.noreply.github.com>
2025-04-08 16:39:14 -0300

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,13 @@ import authRoutes from './routes/authRoutes.js';
 import cors from 'cors';
 
 const app = express();
-const corsOptions = {
-  origin: '*',
-  methods: '*',
-  allowedHeaders: ['Content-Type', 'Authorization'], 
-  credentials: true,
-  'preflightContinue': true
-};
-
-app.use(cors(corsOptions));
+app.use(cors({
+  'allowedHeaders': ['sessionId', 'Content-Type'],
+  'exposedHeaders': ['sessionId'],
+  'origin': '*',
+  'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  'preflightContinue': false
+}));
 app.use(express.json());
 app.use(authRoutes);
 
