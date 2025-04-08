0947fcc8790a37fb480450d5f33c3592171233bb
Luis Gabriel <131626706+LuiisGabriel@users.noreply.github.com>
2025-04-08 17:13:40 -0300

    Update app.js

--- app.js
+++ app.js
@@ -5,18 +5,14 @@ import cors from 'cors';
 const app = express();
 
 const corsOptions = {
-  origin: '*', 
+  origin: 'https://academia-cds.vercel.app', 
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
   allowedHeaders: ['Content-Type', 'Authorization'], 
   credentials: true,
   preflightContinue: false
 };
 
-app.use(cors({
-  origin: 'https://academia-cds.vercel.app',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true
-}));
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(authRoutes);
 
