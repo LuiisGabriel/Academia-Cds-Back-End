11d142e4402f20420a67c81db26bc5cc523e7d4c
Luis Gabriel <131626706+LuiisGabriel@users.noreply.github.com>
2025-04-08 16:51:22 -0300

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,7 +5,11 @@ const router = express.Router();
 const authController = new AuthController();
 
 router.options('*', (req, res) => {
-  res.sendStatus(204);
+  res.set('Access-Control-Allow-Origin', '*');
+  res.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.set('Access-Control-Allow-Credentials', 'true');
+  res.sendStatus(204); 
 });
 
 router.post('/auth/signup', (req, res) => authController.signup(req, res));
