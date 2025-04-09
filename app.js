b69fde1bca23ea9e117d67d09ab4818192208e38
Luis Gabriel <131626706+LuiisGabriel@users.noreply.github.com>
2025-04-09 15:07:40 -0300

    Update app.js

--- app.js
+++ app.js
@@ -4,16 +4,9 @@ import authRoutes from './routes/authRoutes.js';
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
-  );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
-app.use(cors());
+
+app.options('*', cors());
+
 app.use(express.json());
 app.use(authRoutes);
 
