7f60589844eb81d343bad753bef1f956b8c4557b
Luis Gabriel <131626706+LuiisGabriel@users.noreply.github.com>
2025-04-09 16:14:29 -0300

    Update app.js

--- app.js
+++ app.js
@@ -4,13 +4,22 @@ import authRoutes from './routes/authRoutes.js';
 
 const app = express();
 
-const corsOptions = {
-  credentials: true,
-  origin: "*"
-}
+app.options('*', (req, res) => {
+   res.header('Access-Control-Allow-Origin', 'https://academia-cds.vercel.app');
+   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+   res.header('Access-Control-Allow-Credentials', 'true');
+   res.sendStatus(204);
+ });
+ 
+ app.use(cors({
+   origin: '*', 
+   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
+   allowedHeaders: ['Content-Type', 'Authorization'], 
+   credentials: true 
+ }));
 
-app.options('*', cors(corsOptions));
-app.use(cors(corsOptions));
+
 app.use(express.json());
 app.use(authRoutes);
 
