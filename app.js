ae5e242a1d35e1b51f3e743b190bfda86966d8b4
Luis Gabriel <131626706+LuiisGabriel@users.noreply.github.com>
2025-04-09 13:42:34 -0300

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'https://academia-cds.vercel.app');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
