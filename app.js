505d089520df8c3b725a405648b0c651b0ea459e
Luis Gabriel <131626706+LuiisGabriel@users.noreply.github.com>
2025-04-09 16:11:40 -0300

    Update app.js

--- app.js
+++ app.js
@@ -4,9 +4,13 @@ import authRoutes from './routes/authRoutes.js';
 
 const app = express();
 
+const corsOptions = {
+  credentials: true,
+  origin: "*"
+}
 
 app.options('*', cors());
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(authRoutes);
 
