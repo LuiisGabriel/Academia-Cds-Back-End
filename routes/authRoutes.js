e5596b49ea56beac97e7b9c4d2dfa887223bd3d9
Luis Gabriel <131626706+LuiisGabriel@users.noreply.github.com>
2025-04-09 16:47:36 -0300

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,8 +6,8 @@ const router = express.Router();
 const authController = new AuthController();
 
 
-router.post('/auth/signup', (req, res) => authController.signup(req, res));
-router.post('/auth/signin', (req, res) => authController.signin(req, res));
+router.post('/auth/signUp', (req, res) => authController.signup(req, res));
+router.post('/auth/signIn', (req, res) => authController.signin(req, res));
 router.get('/auth/me', (req, res) => authController.getCurrentUser(req, res));
 router.post('/auth/createVideo', (req, res) => authController.createVideo(req, res));
 
