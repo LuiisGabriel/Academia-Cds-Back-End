a34263b300cb0229a35f7221e937c700a0b8142e
Luis Gabriel <131626706+LuiisGabriel@users.noreply.github.com>
2025-04-09 17:21:28 -0300

    Update app.js

--- app.js
+++ app.js
@@ -12,12 +12,15 @@ app.options('*', (req, res) => {
   res.sendStatus(204); 
 });
 
-app.use(cors({
+const corsOrigin = {
   origin: 'https://academia-cds.vercel.app', 
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],  
   allowedHeaders: ['Content-Type', 'Authorization'], 
   credentials: true,  
-}));
+  optionSuccessStatus:200
+};
+
+app.use(cors(corsOrigin));
 app.use(express.json());
 app.use(authRoutes);
 
