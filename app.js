6e6b0bf51d882388e7c0ba76005ca4c20222adad
Luis Gabriel <131626706+LuiisGabriel@users.noreply.github.com>
2025-04-10 13:43:49 -0300

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express();
      res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
      res.header('Access-Control-Allow-Credentials', 'true');
-     res.sendStatus(204); 
+     res.sendStatus(200); 
    });
   
   const corsOrigin = {
